683922ce2b749e79ac2227cfdd61ca1a0ff271d4
John Hunter <john@johnhunter.info>
2019-10-19 16:54:16 +0100

    Add html minifier for production only

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,28 @@
 module.exports = function(eleventyConfig) {
+  const ENV_DEV = process.env.ELEVENTY_ENV === 'dev';
 
 
-    /* Markdown Plugins */
-    let markdownIt = require("markdown-it");
-    let options = {
-      html: true,
-      breaks: true,
-      linkify: true
-    };
-    eleventyConfig.setLibrary("md", markdownIt(options));
+  // Transform Markdown content
+  const markdownIt = require("markdown-it");
+  const options = {
+    html: true, // allow html tags in source
+    breaks: true, // convert line-break to <br>
+    linkify: true, // auto-convert urls to links (uses linkify-it)
+  };
+  eleventyConfig.setLibrary("md", markdownIt(options));
+
+  // minify the html output
+  const htmlmin = require("html-minifier");
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: false, // comments identify the excerpt split marker?
+        collapseWhitespace: !ENV_DEV,
+      });
+    }
+    return content;
+  });
 
   return {
     templateFormats : ["njk", "md"],
