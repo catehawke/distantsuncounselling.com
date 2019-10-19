94caf92b414f2824c18b79ee82e3d1ac4934160e
John Hunter <john@johnhunter.info>
2019-10-19 20:28:30 +0100

    Improve builds
    - refactor css task
    - add clean task
    - disable css/js optimisation on netlify processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,32 @@
 const { src, dest, watch } = require('gulp');
+const clean = require('gulp-clean');
+const gulpIf = require('gulp-if');
 const sass = require("gulp-sass");
 const sassMaps = require('gulp-sourcemaps');
 
 
 const ENV_PROD = process.env.ELEVENTY_ENV === 'prod';
 const sassPath = 'site/sass/*.scss';
+const cleanPath = ['dist/', 'site/css/'];
 
 
 function css () {
-  const destPath = 'site/css';
   const options = {
     outputStyle: ENV_PROD ? 'compressed' : 'expanded',
   };
 
-  if (ENV_PROD) {
-    return src(sassPath)
-      .pipe(sass(options).on('error', sass.logError))
-      .pipe(dest(destPath));
-  }
   return src(sassPath)
-    .pipe(sassMaps.init())
+    .pipe(gulpIf(ENV_PROD, sassMaps.init()))
     .pipe(sass(options).on('error', sass.logError))
-    .pipe(sassMaps.write())
-    .pipe(dest(destPath));
+    .pipe(gulpIf(ENV_PROD, sassMaps.write()))
+    .pipe(dest('site/css'));
 };
 
 exports.build = css;
-exports.watch = function () { watch(sassPath, { ignoreInitial: false }, css) }
+exports.watch = function () {
+  watch(sassPath, { ignoreInitial: false }, css);
+};
+exports.clean = function () {
+  return src(cleanPath, { read: false, allowEmpty: true })
+    .pipe(clean());
+}
