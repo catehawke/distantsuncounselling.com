fdf62974574d01a5ba2cdd48d36c54988fdd25c7
John Hunter <john@johnhunter.info>
2019-11-03 19:33:57 +0000

    Create obfuscate shortcode

---
+++ site/_shortcodes/obfuscate.js
@@ -0,0 +1,20 @@
+/**
+ * Provides Nunjucks shortcode to obfuscate emails etc.
+ * @param {String} text the text to obfuscate
+ * @param {String} scheme  if provided the text will be wrapped in an <a/> tag
+ * @param {String} className <a/> className if scheme provided
+ * @returns {String}
+ */
+module.exports = function obfuscate (text, scheme, className) {
+  if (!scheme) return toEntities(text);
+  const classAttr = className ? `class="${className}" ` : '';
+  const href = `${scheme}:${text}`;
+  return `<a ${classAttr}href="${toEntities(href)}">${toEntities(text)}</a>`;
+}
+
+function toEntities (str) {
+  return Array.from(str)
+  .map(char => `&#${char.charCodeAt(0)};`)
+  .join('');
+}
+
